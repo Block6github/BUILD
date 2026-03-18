7106907aad170e43b5ce6b75b91626de47d7cdc6
Block6github <jonathanbyaruhanga1234567890@gmail.com>
2026-03-18 14:01:11 +0300

    Create physics_engine.py

---
+++ physics_engine.py
@@ -0,0 +1,7 @@
+# Antigravity Physics Engine - Version 1.0
+def apply_gravity(object_weight):
+    gravity_constant = 9.8
+    force = object_weight * gravity_constant
+    print(f"Applying downward force: {force} Newtons")
+
+apply_gravity(83) # Testing with a sample weight
